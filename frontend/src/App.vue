4b0c469feb76b95d036c3e26f865fa52807bd7b9
nibedita6302 <nibedita.6302@gmail.com>
2023-11-27 00:59:27 +0530

    Basic changes to Vue project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="main">
+    <Navbar />
+    <SearchBar />
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Navbar from './components/Navbar.vue'
+import SearchBar from './components/SearchBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    SearchBar
+  }
 }
 
-nav {
-  padding: 30px;
-}
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  /* #main{
+    background-image: url('./assets/background.jpg');
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 100vh;
+  } */
 </style>
