5600769eda179f8f9fb7fdbeba4d12e03206518a
nibedita6302 <nibedita.6302@gmail.com>
2023-11-28 02:43:23 +0530

    Navbar options based on logged user

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -12,19 +12,30 @@
             <router-link active-class="active" class="nav-link" aria-current="page" to="/">Home</router-link>  
           </li>
           <li class="nav-item">
-            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+            <router-link to='#' active-class="active" class="nav-link">Offers</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+            <router-link to='#' v-if="this.user.role=='customer'||this.user.role===null" 
+            active-class="active" class="nav-link">My Cart</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+            <router-link to='#' v-if="this.user.role=='customer'||this.user.role===null" 
+            active-class="active" class="nav-link">Profile</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+            <router-link to='#' v-if="this.user.role=='admin'||this.user.role=='store_manager'"
+             active-class="active" class="nav-link">Approvals </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='#' v-if="this.user.role=='admin'" active-class="active" class="nav-link">
+              Logs & Reports
+            </router-link>
           </li>
           <li class="nav-item">
             <router-link to='/login' active-class="active" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='/login' active-class="active" class="nav-link">Logout</router-link>
           </li>
         </ul>
       </div>
@@ -34,13 +45,23 @@
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  data(){
+    return {
+      user: null
+    }
+  },
+  created(){
+    const user=localStorage.getItem('user');
+    // console.log(user)
+    this.user = JSON.parse(user)[0];
+  }
 }
 </script>
 
 <style scoped>
   .active {
-    border: 1px solid white;
+    border-bottom: 2px solid white;
   }
 </style>
  
