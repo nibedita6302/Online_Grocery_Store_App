d64f051c2016239ad5055fc1a697344a1e54c937
nibedita6302 <nibedita.6302@gmail.com>
2023-11-27 10:36:08 +0530

    All Commit

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <!-- Toggle Button -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link active-class="active" class="nav-link" aria-current="page" to="/">Home</router-link>  
+          </li>
+          <li class="nav-item">
+            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='#' active-class="active" class="nav-link">Features</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to='/login' active-class="active" class="nav-link">Login</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+  .active {
+    border: 1px solid white;
+  }
+</style>
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ProductList />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from '../components/ProductList.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    ProductList
   }
 }
 </script>
