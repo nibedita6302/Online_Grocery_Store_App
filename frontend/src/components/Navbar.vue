6bd3b36f606ee8cb92141ccbb951015472d0e6cb
nibedita6302 <nibedita.6302@gmail.com>
2023-11-28 13:51:13 +0530

    Implemented refreshing on clicking Home in navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -9,17 +9,18 @@
       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link active-class="active" class="nav-link" aria-current="page" to="/">Home</router-link>  
+            <router-link active-class="active" class="nav-link" aria-current="page" to="/" @click="refresh">
+              Home</router-link>  
           </li>
           <li class="nav-item">
             <router-link to='#' active-class="active" class="nav-link">Offers</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='#' v-if="this.user.role=='customer'||this.user.role===null" 
+            <router-link to='#' v-if="this.user.role=='customer'||this.user===null" 
             active-class="active" class="nav-link">My Cart</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='#' v-if="this.user.role=='customer'||this.user.role===null" 
+            <router-link to='#' v-if="this.user.role=='customer'||this.user===null" 
             active-class="active" class="nav-link">Profile</router-link>
           </li>
           <li class="nav-item">
@@ -35,7 +36,7 @@
             <router-link to='/login' active-class="active" class="nav-link">Login</router-link>
           </li>
           <li class="nav-item">
-            <router-link to='/login' active-class="active" class="nav-link">Logout</router-link>
+            <router-link to='/logout' active-class="active" class="nav-link">Logout</router-link>
           </li>
         </ul>
       </div>
@@ -49,6 +50,11 @@ export default {
   data(){
     return {
       user: null
+    }
+  },
+  methods:{
+    refresh(){
+      this.$router.go(0)
     }
   },
   created(){
