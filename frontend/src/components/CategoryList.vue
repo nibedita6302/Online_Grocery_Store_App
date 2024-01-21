560995757c9053cd1c40366b8bda9e04f527a42f
nibedita6302 <nibedita.6302@gmail.com>
2024-01-21 23:45:13 +0530

    Changes in div - card structure

--- frontend/src/components/CategoryList.vue
+++ frontend/src/components/CategoryList.vue
@@ -13,8 +13,8 @@
         </div>
     </div>
     <div id='top' class="row g-2">
-        <div v-for="c in categories" class="col-4">
-            <div v-if="c.product_count>0||allowCRUD()" class="card bg-dark text-white h-100"  >
+        <div v-for="c in filteredCategory" class="col-4">
+            <div class="card bg-dark text-white h-100">
                 <img :src="get_url(c.c_image)" class="card-img-top" :alt="c.c_image">
                 <div class="card-img-overlay">
                     <h3 class="card-title" @click="categoryClicked(c.c_id)">
@@ -22,11 +22,11 @@
                         class="text-white text-decoration-none">{{ c.c_name }}</router-link>
                     </h3>
                 </div>
-                <div v-show="allowCRUD()" class="card-footer text-center">
+                <div v-show="this.GET_USER_ROLE=='admin'" class="card-footer text-center">
                     <button type="submit" class="btn btn-warning me-2" 
                     @click="selectApi_byRole('PUT',c)">
                         <svg id="in-btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                           <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                         </svg>
                     Update</button>
                     <button type="submit" class="btn btn-danger" 
@@ -148,7 +148,10 @@ export default {
         }  
     },
     computed: {
-        ...mapGetters('auth',['GET_USER_ROLE','GET_USER_TOKEN'])
+        ...mapGetters('auth',['GET_USER_ROLE','GET_USER_TOKEN']),
+        filteredCategory(){
+            return this.categories.filter(c => c.product_count>0||this.allowCRUD())
+        }
     },
     mounted() { 
         this.getAllCategory();
@@ -176,8 +179,8 @@ export default {
         position: relative;
     }
     img{
-        max-height: 300px;
-        max-width: 500px;
+        max-height: 250px;
+        max-width: 450px;
     }
     router-link{
         color: white;
