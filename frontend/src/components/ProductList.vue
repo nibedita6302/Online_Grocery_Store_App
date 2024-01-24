48c6926e196cd3d6091f25369424670425c59856
nibedita6302 <nibedita.6302@gmail.com>
2024-01-24 22:46:09 +0530

    Implemented Add to Cart

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -24,8 +24,7 @@
             <div class="card h-100">
                 <img :src="get_url(p.p_image)" class="card-img-top" :alt="p.p_image">
                 <div class="card-body">
-                    <h5 class="card-title" v-if="this.GET_USER_ROLE=='customer'">{{ p.p_name }}</h5>
-                    <h5 v-else>{{ p.p_name }} - ID: {{ p.p_id }}</h5>
+                    <h5 class="card-title">{{ p.p_name }}</h5>
                     <em class="card-text">{{ p.brand }}</em>
                     <p class="card-text">{{p.p_description}} | {{p.p_qty}} {{p.unit}} | ₹{{p.price}} </p>
                     <p v-if="p.stock_remaining>5" class="card-text" style="color:green;">In Stock</p>
@@ -33,9 +32,11 @@
                         Only few left!
                     </p>
                     <p v-else class="card-text" style="color:red;">Out of Stock</p>
-                    <form v-if="this.GET_USER_ROLE=='customer'">
-                        <input type="number" value="1" min="1" :max="p.stock_remaining" required/>
-                        <button class="btn btn-warning" @click="addToCart" :disabled="p.stock_remaining==0">
+                    <form v-if="this.GET_USER_ROLE=='customer'" @submit.prevent="addToCart(p.p_id)">
+                        <input type="number" value="1" min="1" :max="p.stock_remaining"
+                         v-model="this.bought_qty" required/>
+                        <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#formModal3"
+                        :disabled="p.stock_remaining==0">
                             Add to Cart
                         </button>
                     </form>
@@ -70,7 +71,8 @@ export default {
         return {
             show_product_update_form: false,
             action: null,
-            msg: 'Operation Successful'
+            msg: 'Operation Successful',
+            bought_qty: null
         }
     },
     components:{
@@ -105,8 +107,25 @@ export default {
                 this.deleteProduct(p.p_id);
             }
         },
-        addToCart(){
-
+        async addToCart(p_id){
+            await fetch('http://10.0.2.15:8000/api/customer/'+
+                        this.GET_USER_ID+'/add/product/'+p_id+'?auth_token='+this.GET_USER_TOKEN, {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify({'bought_qty':this.bought_qty}),
+                headers:{
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include'
+            }).then((res)=>{
+                if (!res.ok){
+                    throw Error('HTTP Error at Delete Product:',res.status);
+                }
+                return res.json()
+            }).then((data)=>{
+                this.msg = data.message;
+            }).catch((error)=>{console.log(error);})
         },
         async deleteProduct(p_id){
             await fetch('http://10.0.2.15:8000/api/product/'+p_id+
